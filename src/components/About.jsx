1dec891a45170f472c282d284dbd386f63aa0dc8
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 19:55:13 +0100

    Fix introducton text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Tilt from 'react-tilt'
+import {motion} from 'framer-motion';
+
+import {styles} from '../styles';
+import {services} from '../constants';
+import{fadeIn, textVariant} from '../utils/motion';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+<p className={styles.sectionSubText}>Introduction</p>
+<h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.div>
+
+    <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 
+    text-secondary text-[17px] max-w-3x1 leading-[30px]">
+    Test text: Testamente möjliggör till en fördelning av 
+    kvarlåtenskapen i enlighet med testators vilja 
+    framför de regler som anges i arvsordningen. För 
+    det fall att ett giltigt testamente föreligger kan 
+    detta medföra ett partiellt eller totalt åsidosättande 
+    av arvsordningens fördelningsregler. Upprättande av testamente 
+    är således ett sätt att ge den individuella viljan genomslag 
+    framför traditionen med släktarv..
+    </motion.p>
+    </>
   )
 }
 
