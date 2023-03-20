b691ba5004668629b510e8cbde5308785575ce65
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 13:44:36 +0100

    Add navbar with icon and name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+
+import {styles} from '../styles';
+import {navLinks} from '../constants';
+import {logo, menu, close}from '../assets'
 
 const Navbar = () => {
+  const [active,setActive] = useState('')
+
   return (
-    <div>Navbar</div>
+   <nav className={`${styles.paddindX} w-full flex
+    items-center py-5 fixed top-0 z-20 
+    bg-primary`}>
+      <div className="w-full flex justify-between
+       items-center max-w7x1 mx-auto">
+<Link
+to="/"
+className="flex items-center gap-2"
+onClick={() => {
+  setActive("");
+  window.scrollTo(0,0)
+}}>
+<img src ={logo} 
+alt="logo" 
+className="w-9 h-9 object-contain"/>
+<p className="text-white text-[18px]
+ font-bold cursor-pointer">Anna <span className="sm:block hidden"> Axelsson
+  </span></p>
+</Link>
+<p className="text-red-500">asdsa</p>
+      </div>
+    </nav>
   )
 }
 
