9b42d61e053da7ad1045f84cc3506acf9306140f
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 17:14:53 +0100

    Add 3d model computer and camera angles

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -12,11 +12,30 @@ const Computers = () => {
    <mesh>
     <hemisphereLight intensity={0.15}
     groundColor="black"/>
-    <pintLight intensity={1}/>
+    <pointLight intensity={1}/>
     <primitive
     object={computer.scene}/>
    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return(
+  <Canvas
+  frameloop="demand"
+  shadows
+  camera={{ position: [20, 3, 5], fov: 25}}
+  gl={{ preserveDrawingBuffer: true}}>
+<Suspense fallback={<CanvasLoader/>}>
+  <OrbitControls 
+  enableZoom={false}
+  maxPolarAngle={Math.PI / 2}
+  minPolarAngle={Math.PI / 2}/>
+  <Computers/>
+</Suspense>
+<Preload all/>
+  </Canvas>
+  )
+}
+
+export default ComputersCanvas;
