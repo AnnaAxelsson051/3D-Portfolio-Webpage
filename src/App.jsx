14c9bdec8796699990190e42bdf5d3e8aaf584a7
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 13:41:17 +0100

    Add project skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 
-const App = () => {
 
+import { BrowserRouter} from 'react-router-dom';
+
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+
+
+const App = () => {
   return (
-    <div>
-    3d Dev Portfolio
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover">
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className="relative z-0">
+        <Contact/>
+        <StarsCanvas/>
+      </div>
     </div>
+    </BrowserRouter>
+ 
   )
 }
 
 export default App
+
