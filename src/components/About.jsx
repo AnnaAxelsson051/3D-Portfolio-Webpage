07fc28c3cc64db0191e73b50e72990a8d8755b01
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 20:25:03 +0100

    Add informative cards and styling + tilt motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,26 @@ import {styles} from '../styles';
 import {services} from '../constants';
 import{fadeIn, textVariant} from '../utils/motion';
 
+const ServiceCard = ({index, title, icon}) =>{
+  return(
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div variants={fadeIn("right", "spring",0.5 * index, 0.75)} 
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+<div options={{
+  max: 45,
+  scale: 1,
+  speed: 450
+}} className="bg-tertiary rounded-[20px] 
+py-5 px-12 min-h-[280px] flex 
+justify-evenly items-center flex-col">
+<img src={icon} alt={title} className="w-16 h-16 object-contain"/>
+<h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+</div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
 const About = () => {
   return (
     <>
@@ -25,6 +45,12 @@ const About = () => {
     är således ett sätt att ge den individuella viljan genomslag 
     framför traditionen med släktarv..
     </motion.p>
+    <div className="mt-20 flex flex-wrap gap-10">
+      {services.map((service, index) =>(
+        <ServiceCard key={service.title} index={index} {...service}/>
+      ))}
+
+    </div>
     </>
   )
 }
