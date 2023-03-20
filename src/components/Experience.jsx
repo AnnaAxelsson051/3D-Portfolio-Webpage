234e87163f127b4a1fcc205a2171a16711e5b224
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 22:13:27 +0100

    Center icons

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,7 +15,7 @@ const ExperienceCard = ({experience}) =>(
   date={experience.date}
   iconStyle={{background:experience.iconBg}}
   icon={
-  <div>
+  <div className="flex justify-center items-center w-full h-full">
     <img 
     src={experience.icon}
     alt={experience.company_name}
