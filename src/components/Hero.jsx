891e46d22b9b48531ed083e9c0bc2ed62b99bfb2
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 16:34:49 +0100

    Add heading and presentation text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,13 @@ const Hero = () => {
 <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
 <div className="w-1 sm:h-80 h-40 violet-gradient"/>
         </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Anna</span></h1>
+        <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop user interfaces and web <br className="sm:block hidden"/> applications </p>
+        </div>
       </div>
+
+      <ComputerCanvas/>
     </section>
   )
 }
