f67206aa200cbafab70a5c2b48844ffe03c6e6bb
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 21:16:03 +0100

    Alter text sizes ans placement

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import {motion} from 'framer-motion';
 
 import {styles} from '../styles';
 import {services} from '../constants';
-import{fadeIn, textVariant} from '../utils/motion';
+import {fadeIn, textVariant} from '../utils/motion';
 import {SectionWrapper} from '../hoc'
 
 const ServiceCard = ({index, title, icon}) =>{
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute 
-      inset-0 top-[120px] max-w-7xl max auto flex 
+      inset-0 top-[70px] max-w-7xl mx-auto flex 
       flex-row items-start gap-5`}>
         <div className="flex flex-col 
         justify-center items-center mt-5">
@@ -24,7 +24,7 @@ const Hero = () => {
       bottom-32 w-full flex justify-center 
       items-center">
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 
+          <div className="w-[30px] h-[56px] rounded-3xl border-4 
           border-secondary flex justify-center items-start p-2">
             <motion.dev 
             animate={{
