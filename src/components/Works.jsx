888067a2a49f9b3e578023bc28a928405f599d67
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 23:56:53 +0100

    Add imports for worksection

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
 
-const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
-
-export default Works
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
