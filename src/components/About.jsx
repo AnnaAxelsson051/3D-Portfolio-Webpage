eedd8b50745f944920c79a7e0a276adf3849a873
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 20:49:05 +0100

    Add higher order coponent for wrapping sections for positioning

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import {motion} from 'framer-motion';
 import {styles} from '../styles';
 import {services} from '../constants';
 import{fadeIn, textVariant} from '../utils/motion';
+import {SectionWrapper} from '../hoc'
 
 const ServiceCard = ({index, title, icon}) =>{
   return(
@@ -55,4 +56,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper (About, "about")
