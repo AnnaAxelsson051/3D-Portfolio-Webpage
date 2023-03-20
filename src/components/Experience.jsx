2a8f1c504e226d851e83f09728a93261509df23e
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-20 22:57:47 +0100

    Add template for tech-balls positioning, size

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -24,8 +24,22 @@ const ExperienceCard = ({experience}) =>(
   }
   >
 <div>
-  <h3 className="text-white text-[24px font-bold]">{experience.title}</h3>
+  <h3 
+  className="text-white text-[24px] font-bold">{experience.title}</h3>
+<p 
+className="text-secondary text-[16px] font-semibold" 
+style={{margin:0}}>{experience.company_name}</p>
 </div>
+<ul className="mt-5 list-disc ml-5 space-y-2">{experience.points.map((point,index) => (
+  <li 
+  key={`experience-point-${index}`}
+  className="text-white-100 text-[14px] pl-1 tracking-wider"
+  >
+    {point}
+  </li>
+))}
+
+</ul>
   </VerticalTimelineElement>
 )
 
