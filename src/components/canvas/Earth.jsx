b20c4f360d55714eba4a9071e05173cf6b7c2c8e
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-21 15:16:08 +0100

    Add animations for earth model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -10,4 +10,25 @@ const Earth = () => {
   )
 }
 
+const EarthCanvas =()=>{
+  return(
+    <Canvas 
+    shadows
+    frameLoop='demand'
+    gl={{preserveDrawingBuffer: true}}
+    camera={{}}>
+
+      <Suspense
+      fallback={<CanvasLoader/>}>
+<OrbitControls
+autoRotateenableZoom={false}
+maxPolarAngle={Math.PI / 2}
+minPolarAngle={Math.PI / 2}
+/>
+     </Suspense>
+
+    </Canvas>
+  )
+}
+
 export default Earth
