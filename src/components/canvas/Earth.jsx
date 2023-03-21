5f55a27db3463d2212bed46c5d232d14396f7365
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-21 15:25:03 +0100

    Add earth object

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,9 +4,17 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
+
+
 const Earth = () => {
+  const earth = useGLTF('./planet/scene.gltf')
+
   return (
-    <div>Earth</div>
+   <primitive
+   object={earth.scene}
+   scale={2.5}
+   position-y={0}
+   rotation-y={0}/>
   )
 }
 
@@ -25,10 +33,11 @@ autoRotateenableZoom={false}
 maxPolarAngle={Math.PI / 2}
 minPolarAngle={Math.PI / 2}
 />
+<Earth/>
      </Suspense>
 
     </Canvas>
   )
 }
 
-export default Earth
+export default EarthCanvas;
