d69ae63a7e2f77c4fd9f4b572d90068bb7c1027f
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-21 23:08:52 +0100

    Alter colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,32 +10,33 @@ const Hero = () => {
       flex-row items-start gap-5`}>
         <div className="flex flex-col 
         justify-center items-center mt-5">
-<div className="w-5 h-5 rounded-full bg-[#915eff]"/>
-<div className="w-1 sm:h-80 h-40 violet-gradient"/>
+{/*<div className="w-5 h-5 rounded-full bg-white"/>*/}
+{/*<div className="w-1 sm:h-80 h-40 violet-gradient"/>*/}
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Anna</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Welcome to my page <span className="text-white"></span></h1>
         <p className={`${styles.heroSubText} text-white-100`}>I develop user interfaces and web <br className="sm:block hidden"/> applications </p>
         </div>
       </div>
+      {/*lila: text-[#915eff] */}
 
       <ComputersCanvas/>
       <div className="absolute xs:bottom-2 
       bottom-32 w-full flex justify-center 
       items-center">
         <a href="#about">
-          <div className="w-[29px] h-[46px] rounded-3xl border-4 
+          <div className="w-[33px] h-[46px] rounded-3xl border-4 
           border-secondary flex justify-center items-start p-2">
             <motion.dev 
             animate={{
-              y: [0, 20, 0]
+              y: [0, 15, 0]
             }}
             transition={{
             duration: 1.5,
             repeat: Infinity,
             repeatType: 'loop'
             }}
-            className="w-3 h-3 rounded-full bg-secondary mb-1"/>
+            className="w-5 h-2 rounded-full bg-secondary mb-1"/>
           </div>
         </a>
       </div>
