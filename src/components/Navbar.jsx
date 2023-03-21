6950263d8013f666c068d219bb389e1c3ec9dbba
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-21 23:13:45 +0100

    Remove logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+         {/* <img src={logo} alt='logo' className='w-9 h-9 object-contain' />*/}
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Anna &nbsp;
             <span className='sm:block hidden'> Axelsson</span>
