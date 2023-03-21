3fbf936e3d1073df68f564f57f5523229681f8de
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-21 15:40:23 +0100

    Add imports

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = () => {
   return (
