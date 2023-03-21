0c21ddf0487ad1c354c9bf9dca0ca10c7740856e
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-21 16:47:31 +0100

    Fix bugs

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,12 +6,10 @@ import * as random from "maath/random/dist/maath-random.esm";
 const Stars = (props) => {
  const ref = useRef();
 
-
-
     const sphere = random.inSphere(new Float32Array 
       (5000), {radius: 1.2})
       return(
-        <group rotation={[0,0, MATH.PI / 4]}>
+        <group rotation={[0,0, Math.PI / 4]}>
           <Points ref={ref} positions={sphere} stride={3}
            frustumCulled {...props}>
             <PointMaterial
@@ -28,7 +26,7 @@ const Stars = (props) => {
 const StarsCanvas = () =>{
   return(
     
-    <div className="w-full h-full absolute inset-0 
+    <div className="w-full h-auto absolute inset-0 
     z-[-1]">
       <Canvas camera={{position: [0, 0, 1]}}>
         <Suspense fallback={null}>
@@ -39,4 +37,4 @@ const StarsCanvas = () =>{
   )
 }
 
-export default Stars
+export default StarsCanvas
