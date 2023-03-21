a3fc9fb7e676757df9806919050f19d2c8d2d724
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-21 15:46:39 +0100

    Add canvas for background stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -3,9 +3,23 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = () => {
+const Stars = (props) => {
   return (
     <div>Stars</div>
+  )
+}
+
+const StarsCanvas = () =>{
+  return(
+    
+    <div className="w-full h-full absolute inset-0 
+    z-[-1]">
+      <Canvas camera={{position: [0, 0, 1]}}>
+        <Suspense fallback={null}>
+          <Stars/>
+
+        </Suspense>
+      </Canvas>  </div>
   )
 }
 
