6d0094a8657092eceb4187b2396cae1c8fa4f718
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-21 17:16:04 +0100

    Fix bug

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,40 +4,41 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-
-
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf')
+  const earth = useGLTF("./planet/scene.gltf");
 
   return (
-   <primitive
-   object={earth.scene}
-   scale={2.5}
-   position-y={0}
-   rotation-y={0}/>
-  )
-}
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
+};
 
-const EarthCanvas =()=>{
-  return(
-    <Canvas 
-    shadows
-    frameLoop='demand'
-    gl={{preserveDrawingBuffer: true}}
-    camera={{}}>
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
 
-      <Suspense
-      fallback={<CanvasLoader/>}>
-<OrbitControls
-autoRotateenableZoom={false}
-maxPolarAngle={Math.PI / 2}
-minPolarAngle={Math.PI / 2}
-/>
-<Earth/>
-     </Suspense>
-
+        <Preload all />
+      </Suspense>
     </Canvas>
-  )
-}
+  );
+};
 
 export default EarthCanvas;
