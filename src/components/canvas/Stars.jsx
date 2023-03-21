61467d3fe5bd8f25e7920507c9069fcdd90a5b0b
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-21 16:44:03 +0100

    Add stars and rotation of stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -4,9 +4,25 @@ import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
-  return (
-    <div>Stars</div>
-  )
+ const ref = useRef();
+
+
+
+    const sphere = random.inSphere(new Float32Array 
+      (5000), {radius: 1.2})
+      return(
+        <group rotation={[0,0, MATH.PI / 4]}>
+          <Points ref={ref} positions={sphere} stride={3}
+           frustumCulled {...props}>
+            <PointMaterial
+            transperant
+            color="#f272c8"
+            size={0.002}
+            sizeAttenuation={true}
+            depthWrite={false}/>
+            </Points>
+        </group>
+      )
 }
 
 const StarsCanvas = () =>{
