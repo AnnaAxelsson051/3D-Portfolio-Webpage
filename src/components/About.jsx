58dcf3e4f007b78e23ed058cc3e1b5158c1a4ac1
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-21 23:16:28 +0100

    Alter bg color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const ServiceCard = ({index, title, icon}) =>{
   max: 45,
   scale: 1,
   speed: 450
-}} className="bg-tertiary rounded-[20px] 
+}} className="bg-black rounded-[20px] 
 py-5 px-12 min-h-[280px] flex 
 justify-evenly items-center flex-col">
 <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
